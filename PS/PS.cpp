2b7b8b773c1192bba9c2ee851be486a0b8efe2c3
seong bynn jo <bearsharks02@gmail.com>
2020-12-18 17:08:43 +0900

    1300-1400

---
+++ PS/PS.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int ans = 0;
+    while (n > 0) {
+        ans++;
+        n /= 2;
+    }
+    cout << ans;
+    return 0;
+}
